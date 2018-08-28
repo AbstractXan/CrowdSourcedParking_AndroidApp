abf279287b8813e8ed1fbb276f025453576c1e4e
Priyansh Sangule <priyansh0506@gmail.com>
2018-08-28 09:56:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/psangule/ultrabakchodi/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.psangule.ultrabakchodi
+
+import android.content.Context
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+ class MainActivity : AppCompatActivity() {
+
+    var mContext: Context? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
+
